42d28dbc825ad66d231a052e50cf4733109e6501
Hamish Henare <hamishjustin7@gmail.com>
2021-07-21 14:57:36 +1200

    form is finished

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -2,7 +2,14 @@ import React, { Component } from 'react'
 
 export default class Form extends Component {
 
+    
+
     formSubmit(event) {
+        // if (document.getElementsByClassName('form__select')[0].value === "Default") {
+        //     console.log('bbeep')
+        //     event.preventDefault()
+
+        // }
         alert('The form was submitted')
         event.preventDefault()
     }
@@ -11,10 +18,11 @@ export default class Form extends Component {
         return (
             <form onSubmit={this.formSubmit} className="form">
                 Form title
-                <input className="form__input" placeholder="First name" type="text" name="firstName"/>
-                <input className="form__input" placeholder="Last name" type="text" name="lastName" />
-                <input className="form__input" placeholder="Email address" type="email" name="email" />
-                <select className="form__input form__select" name="city">
+                <input required  className="form__input" placeholder="First name" type="text" name="firstName"/>
+                <input  required className="form__input" placeholder="Last name" type="text" name="lastName" />
+                <input required  className="form__input" placeholder="Email address" type="email" name="email" />
+                <select className="form__input form__select" name="city" required>
+                    <option value="Default"disabled selected>City</option>
                     <option value="Auckland">Auckland</option>
                     <option value="Wellington">Wellington</option>
                     <option value="Hamilton">Hamilton</option>
